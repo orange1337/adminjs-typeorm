e3ecfc1ed09d0c91191a66a219503e99bfdd680c
Rost <rosman12@gmail.com>
2024-07-01 16:47:44 +0300

    fix typeorm filter by relations

--- src/utils/filter/filter.converter.ts
+++ src/utils/filter/filter.converter.ts
@@ -16,12 +16,30 @@ export const convertFilter = (
   Object.entries(filters ?? {}).forEach(([fieldKey, filter]) => {
     const parser = parsers.find((p) => p.isParserForType(filter))
 
+    let customFilterKey = ''
+    let customFilterValue = ''
     if (parser) {
       const { filterValue, filterKey } = parser.parse(filter, fieldKey)
-      where[filterKey] = filterValue
+      customFilterKey = filterKey
+      customFilterValue = filterValue
     } else {
       const { filterValue, filterKey } = DefaultParser.parse(filter, fieldKey)
-      where[filterKey] = filterValue
+      customFilterKey = filterKey
+      customFilterValue = filterValue
+    }
+
+    const fieldKeyParts = customFilterKey.split('.')
+    if (fieldKeyParts?.length > 1) {
+      fieldKeyParts.reduce((acc, key, index) => {
+        if (index === fieldKeyParts.length - 1) {
+          acc[key] = customFilterValue
+        } else {
+          acc[key] = {}
+        }
+        return acc[key]
+      }, where)
+    } else {
+      where[customFilterKey] = customFilterValue
     }
   })
 

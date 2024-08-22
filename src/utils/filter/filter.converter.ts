ced196e248620c947dc60face6d2482a741fb836
Rost <rosman12@gmail.com>
2024-08-22 12:08:48 +0300

    add support for or where filter

--- src/utils/filter/filter.converter.ts
+++ src/utils/filter/filter.converter.ts
@@ -1,47 +1,73 @@
-import { Filter } from 'adminjs'
+import { Filter, FilterElement, unflatten } from 'adminjs'
 import { BaseEntity, FindOptionsWhere } from 'typeorm'
 import { DefaultParser } from './default-filter.parser'
 import { parsers } from './filter.utils'
 
+function customizeFilter(where: any, fieldKey: string, filter: FilterElement): any {
+  const parser = parsers.find((p) => p.isParserForType(filter))
+  let customFilterKey = ''
+  let customFilterValue = ''
+
+  if (parser) {
+    const { filterValue, filterKey } = parser.parse(filter, fieldKey)
+    customFilterKey = filterKey
+    customFilterValue = filterValue
+  } else {
+    const { filterValue, filterKey } = DefaultParser.parse(filter, fieldKey)
+    customFilterKey = filterKey
+    customFilterValue = filterValue
+  }
+
+  const fieldKeyParts = customFilterKey.split('.')
+  const unflattenQuery = unflatten({ [customFilterKey]: customFilterValue })
+
+  // eslint-disable-next-line no-param-reassign
+  where[fieldKeyParts[0]] = unflattenQuery[fieldKeyParts[0]]
+
+  return where
+}
+
 export const convertFilter = (
   filterObject?: Filter,
-): FindOptionsWhere<BaseEntity> => {
+): FindOptionsWhere<BaseEntity> | FindOptionsWhere<BaseEntity>[] => {
   if (!filterObject) {
     return {}
   }
 
   const { filters } = filterObject ?? {}
   const where = {}
+  const orWhere = {}
 
   Object.entries(filters ?? {}).forEach(([fieldKey, filter]) => {
-    const parser = parsers.find((p) => p.isParserForType(filter))
-
-    let customFilterKey = ''
-    let customFilterValue = ''
-    if (parser) {
-      const { filterValue, filterKey } = parser.parse(filter, fieldKey)
-      customFilterKey = filterKey
-      customFilterValue = filterValue
-    } else {
-      const { filterValue, filterKey } = DefaultParser.parse(filter, fieldKey)
-      customFilterKey = filterKey
-      customFilterValue = filterValue
+    const isOrFilter = filter?.['isOrFilter']
+    if (!isOrFilter) {
+      return
     }
 
-    const fieldKeyParts = customFilterKey.split('.')
-    if (fieldKeyParts?.length > 1) {
-      fieldKeyParts.reduce((acc, key, index) => {
-        if (index === fieldKeyParts.length - 1) {
-          acc[key] = customFilterValue
-        } else {
-          acc[key] = {}
-        }
-        return acc[key]
-      }, where)
-    } else {
-      where[customFilterKey] = customFilterValue
-    }
+    orWhere[fieldKey] = customizeFilter({}, fieldKey, filter)
   })
+
+  Object.entries(filters ?? {}).forEach(([fieldKey, filter]) => {
+    const isOrFilter = filter?.['isOrFilter']
+    if (isOrFilter) {
+      return
+    }
+
+    customizeFilter(where, fieldKey, filter)
+  })
+
+  const orWhereValues = Object.values(orWhere)
+
+  if (orWhereValues?.length) {
+    orWhereValues.forEach((orFilter: any) => {
+      Object.entries(where).forEach(([key, value]) => {
+        // eslint-disable-next-line no-param-reassign
+        orFilter[key] = value
+      })
+    })
+
+    return orWhereValues as FindOptionsWhere<BaseEntity>[]
+  }
 
   return where
 }
